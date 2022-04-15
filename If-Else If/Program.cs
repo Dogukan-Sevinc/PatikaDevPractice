e16fa82543265e86b7ea8bb31a8b13e2c1ca97d6
Dogukan Sevinc <101670829+Dogukansvnc@users.noreply.github.com>
2022-04-15 09:10:57 +0300

    If-Else Yapısı ve Ternary If

---
+++ If-Else If/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+                if(time>=6 && time<11)
+                    System.Console.WriteLine("Günaydın!");
+                else if (time<=18)
+                    System.Console.WriteLine("İyi Günler!");
+                else 
+                    Console.WriteLine("İyi Geceler");
+
+                string sonuc= time<=18 ? "İyi Günler" : "İyi Geceler";
+                Console.WriteLine(sonuc);
+
+                sonuc = time>=6 && time<11 ? "Günaydın!" : time<=18 ? "İyi Günler!" : "İyi Geceler!";
+                Console.WriteLine(sonuc);
+
+        }
+    }
+}
