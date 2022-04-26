7b26395127fc40a54ceb4851ee7e22111b00cbc2
Dogukan Sevinc <dogukansvnc13@gmail.com>
2022-04-26 22:36:23 +0300

    While ve ForEach Döngüleri

--- WhileForeach/Program.cs
+++ WhileForeach/Program.cs
@@ -6,7 +6,16 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lütfen bir sayi giriniz!");
+            int toplam = 0;
+            int sayac = 1;
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine("1'den sayiya kadar olan sayilarin ortalaması: {0}", (toplam / sayi));
         }
     }
 }
