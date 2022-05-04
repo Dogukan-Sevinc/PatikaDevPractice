84bdac2b5ebd2fc87c359313a66ee817733ff2c0
Dogukan Sevinc <dogukansvnc13@gmail.com>
2022-05-04 14:49:02 +0300

    MethodsOverloading

--- Methods Overloading/Program.cs
+++ Methods Overloading/Program.cs
@@ -8,9 +8,9 @@ namespace Methods // Note: actual namespace depends on the project name.
         {
             //out parametreler
             string sayi="999";
-            int outSayi;
+            
 
-            bool sonuc = int.TryParse(sayi, out outSayi);
+            bool sonuc = int.TryParse(sayi, out int outSayi);
             if(sonuc)
             {
                 Console.WriteLine("Başarılı!");
@@ -19,6 +19,48 @@ namespace Methods // Note: actual namespace depends on the project name.
             else {
                 Console.WriteLine("Başarısız!");
             }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(4,5, out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            //Metot Aşırı Yükleme - Overloading
+
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade)); 
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Zikriye"+"Ürkmez"); 
+
+            //Metot Imzası
+            // metotAdi + parametre sayisi + parametre          
+        }
+
+        class Metotlar
+        {
+             public void Topla(int a, int b, out int toplam)
+             {
+                toplam = a+b;
+             }
+
+             public void EkranaYazdir(string veri)
+             {
+                 Console.WriteLine(veri);
+             }
+
+             public void EkranaYazdir(int veri)
+             {
+                 Console.WriteLine(veri);
+             }
+
+             public void EkranaYazdir(int veri, int veri2)
+             {
+                 Console.WriteLine(veri);
+             }
+
+             public void EkranaYazdir(string veri1, string veri2)
+             {
+                 Console.WriteLine(veri1+veri2);
+             }
         }
     }
 }
